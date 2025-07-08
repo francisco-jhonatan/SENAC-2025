0ec786cf8b8e72717cc018ff0857954099c6e8a9
francisco-jhonatan <franciscojhonatan0077@gmail.com>
2025-07-07 21:53:10 -0300

    Jogo-final

--- front-end/pages/javascript/jogo.js
+++ front-end/pages/javascript/jogo.js
@@ -1,11 +1,39 @@
 
+/*var container = document.getElementById('container')
+
 let square = document.createElement('div')
 square.className = 'square'
 
-document.body.appendChild(square)
+container.appendChild(square)
 
-square.addEventListener('click', () => {
-    let a = square
+square.addEventListener('click', ()=>{
+    let a = document.createElement('div')
+    a.className = 'square'
     a.style.backgroundColor = "black"
-    document.body.appendChild(a)
-})
+    container.appendChild(a)
+    })*/
+
+
+var container = document.getElementById('container')
+flag = true
+function CreateSquare(){
+    if(document.querySelector('.square')!==null){
+        document.querySelector('.square').remove()
+    }
+    let a = document.createElement('div')
+    a.className = 'square'
+    a.addEventListener('click', ()=>{
+        CreateSquare()
+    })
+    if(flag){
+        a.style.backgroundColor = "blue"
+    } else{
+        a.style.backgroundColor = "red"
+    }
+    flag = !flag
+    container.appendChild(a)
+}
+
+CreateSquare()
+
+alert(Math.random*100)
