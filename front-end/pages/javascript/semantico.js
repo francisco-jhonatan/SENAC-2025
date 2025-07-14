8d2249846fa9c0200914175adcd025c29aec8631
francisco-jhonatan <franciscojhonatan0077@gmail.com>
2025-07-13 23:33:26 -0300

    String + semântico

---
+++ front-end/pages/javascript/semantico.js
@@ -0,0 +1,9 @@
+var right = document.querySelector('.imagem')
+
+for(let i = 0;i< 1; i++){
+    right.innerHTML +=`
+            <div class="card">
+            <img src="../images/foto-do-computador.avif" alt="">
+        </div>
+    `
+}
