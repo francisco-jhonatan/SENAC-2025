3657a93ec9e720faceec01da6337dfee92fb2c37
francisco-jhonatan <franciscojhonatan0077@gmail.com>
2025-07-16 18:14:22 -0300

    beecrowd

--- front-end/script.js
+++ front-end/script.js
@@ -15,9 +15,9 @@
 //let b = 2; // Escopo local, não pode ser acessada fora do espoco
 //const c = 3; // Imultável, pode ser lida, mas não pode ser reescrita
 //{
-   // alert (b); // ReferenceError
-   // let b = 5;
-   //alert(b); // 5
+// alert (b); // ReferenceError
+// let b = 5;
+//alert(b); // 5
 //}
 //alert(c); // 2
 
@@ -25,3 +25,42 @@
 
 //var boo = false
 //alert(2<5) // O operador de igualdade ===; outros operadores: <,>,<=,>=.
+
+var a = 10
+let b = 20
+const c = 30
+
+//alert(a + " " + b + " " + c)
+
+{
+   var a = 1
+   let b = 2
+   var d = 5
+  // alert(a + " " + b + " " + c)
+}
+
+//alert(a + " " + b + " " + c)
+
+//alert(d)
+
+//var bul = 36 <= 35 && 20 < 30
+//alert(bul)
+
+//if(a===b){
+ //  alert('verdadeiro')
+//} else {
+ //  alert("Falso")
+//}
+
+var vetor = [1,2,3,4,5,6,7,8,9,10]
+ //          0|1|2|3|4|5|6|7|8|9
+//alert(vetor[vetor.length-1])
+
+vetor.push(11)  //adiciona item no fim do array
+vetor.pop()     //remove último valor do array
+vetor.unshift(0)//adiciona no início do array
+vetor.shift()   //remove o primeiro item do array
+
+for(let i = 0;i < vetor.length;i++){
+   alert(vetor[i])
+}
