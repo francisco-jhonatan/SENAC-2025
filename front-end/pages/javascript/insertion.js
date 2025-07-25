1c4d1d6616743823d2b88d2e3d836aa92afa0ad8
francisco-jhonatan <franciscojhonatan0077@gmail.com>
2025-07-25 17:56:00 -0300

    insertion.js

--- front-end/pages/javascript/insertion.js
+++ front-end/pages/javascript/insertion.js
@@ -1,13 +1,16 @@
 function insertionSort(arr) {
-  for (let i = 1; i < arr.length; i++) {
-    let atual = arr[i];        // Elemento atual a ser comparado
-    let j = i - 1;
+  for (let i = 1; i < arr.length; i++) { //Começa no índice 1 (o segundo número)
+    //  e vai até o final da lista.
+    let atual = arr[i];  // Guarda o número que vai ser comparado.
+    let j = i - 1; //Começa comparando com o da esquerda.
 
-    // Move os elementos maiores que 'atual' uma posição à frente
+
     while (j >= 0 && arr[j] > atual) {
       arr[j + 1] = arr[j];
       j--;
-    }
+    } // Se o número da esquerda for maior que o atual,
+     //  ele desce uma posição.
+    //Vai repetindo até achar o lugar certo para o número entrar.
 
     arr[j + 1] = atual; // Insere o elemento na posição correta
   }
@@ -15,12 +18,11 @@ function insertionSort(arr) {
   return arr; // Retorna o array ordenado
 }
 
-// Exemplo de uso:
-const numeros = [5, 3, 8, 4, 2];
-const ordenado = insertionSort(numeros);
+const numeros = [5, 3, 8, 4, 2]; // array números
+const ordenado = insertionSort(numeros); // passa o array números para a função
 console.log("Array ordenado:", ordenado);
 
 
 
-// Resultado esperado
+// Resultado:
 // Array ordenado: [2, 3, 4, 5, 8]//
