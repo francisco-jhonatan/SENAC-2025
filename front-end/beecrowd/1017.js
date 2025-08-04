c4969a8e1f2dd47627ff44c2b91d61787dbed780
francisco-jhonatan <franciscojhonatan0077@gmail.com>
2025-08-04 17:23:42 -0300

    beecrowd.js

---
+++ front-end/beecrowd/1017.js
@@ -0,0 +1,13 @@
+var input = require('fs').readFileSync('/Users/Aluno/Documents/GitHub/SENAC-2025/front-end/beecrowd/stdin', 'utf8');
+var lines = input.split('\n'); //lines é um array
+//=====================================================
+
+let tempoGasto = parseInt(lines.shift())
+let velocidadeMedia = parseInt(lines.shift())
+let kml = 12
+
+let distanciaPercorrida = tempoGasto * velocidadeMedia
+
+let litros = distanciaPercorrida / kml
+
+console.log(`${litros.toFixed(3)}`)
